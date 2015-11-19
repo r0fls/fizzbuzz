f5c703fe5f09cf57e3381cb3bf567482ecdc57b0
Raphael Deem <raphael.deem@gmail.com>
2015-11-19 14:55:17 -0800

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
-d = x = [None,None,'fizz',None,'buzz','fizz',None,None,'fizz','buzz',None,'fizz',None,None,'fizzbuzz']
+d = x = [None, None, 'fizz', None, 'buzz', 'fizz', None, None, 'fizz', 'buzz', None, 'fizz', None, None, 'fizzbuzz']
 fizzbuzz = ((x[(i-1) % 15] or i) for i in range(1,100))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     for i in fizzbuzz:
         print i
