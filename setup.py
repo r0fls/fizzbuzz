10277628a49093a7548cd72b3fc5363d26ea6efe
r0fls <raphael.deem@gmail.com>
2015-11-19 15:02:31 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'fizzbuzz',
+  packages = ['fizzbuzz'],
+  version = '0.1',
+  description = 'python -c 'from fizzbuzz import fizzbuzz; print(list(fizzbuzz))'
+,
+  author = 'Raphael Deem',
+  author_email = 'raphael.deem@gmail.com',
+  url = 'https://github.com/r0fls/fizzbuzz',
+  download_url = 'https://github.com/r0fls/fizzbuzz/tarball/0.1',
+  keywords = ['fizzbuzz', 'fizz', 'buzz', 'interview'], # arbitrary keywords
+  classifiers = [],
+)
